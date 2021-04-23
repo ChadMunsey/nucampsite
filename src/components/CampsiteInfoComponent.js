896ed72dd196a0e3c9258000d1c71e624546ea69
Chad Munsey <munsey.chad@gmail.com>
2021-04-22 20:55:29 -0500

    Controlled Forms

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class CampsiteInfo extends Component {
     constructor(props) {
@@ -15,7 +16,6 @@ class CampsiteInfo extends Component {
                 <Card>
                     <CardImg top src={campsite.image} alt={campsite.name} />
                     <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
@@ -47,8 +47,18 @@ class CampsiteInfo extends Component {
             return (
                 <div className="container">
                     <div className="row">
+                        <div className="col">
+                            <Breadcrumb>
+                                <BreadcrumbItem><Link to="/directory">Directory</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{this.props.campsite.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <h2>{this.props.campsite.name}</h2>
+                            <hr />
+                        </div>
+                    </div>                    
+                    <div className="row">
                         {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
+                        {this.renderComments(this.props.comments)}
                     </div>
                 </div>
             );
